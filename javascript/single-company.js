1684a32da93a3513dec1e8eeff0579aba51d70ae
Minh Bao <minhbao@Minhs-MacBook-Air.local>
2021-04-09 11:25:48 -0600

    updated singlee-company.js file

--- javascript/single-company.js
+++ javascript/single-company.js
@@ -4,22 +4,51 @@ document.addEventListener("DOMContentLoaded", function () {
     const urlParams = new URLSearchParams(window.location.search);
     const myParam = urlParams.get('symbol');
     const companyAPI = "api-companies.php?symbol=" + myParam;
+    const singleCompany = document.querySelector(".singleCompany");
+    const buttons = document.querySelector(".button");
 
-    console.log("working");
     console.log(companyAPI);
 
     fetchSingleCompany();
 
     function fetchSingleCompany() {
-        console.log("yes");
         fetch(companyAPI)
             .then(response => response.json())
             .then(data => {
                 console.log(data);
+                outputSingleCompany(data);
         })
             .catch((error) => {
                 console.log(error);
         });
     }
+
+    function outputSingleCompany(company) {
+
+        const fields = [
+            ["Symbol", company[0].symbol], 
+            ["Name", company[0].name], 
+            ["Sector", company[0].sector], 
+            ["Sub-Industry", company[0].subindustry], 
+            ["Address", company[0].address],
+            ["Exchange", company[0].exchange],
+            ["Website", company[0].website],
+            ["Description", company[0].description]
+        ];
+        fields.forEach((f) => {
+            let divi = document.createElement("div");
+            singleCompany.appendChild(divi);
+            divi.innerHTML = `${f[0]}: ${f[1]}`;
+        })
+    }
+
+    const favButton = document.createElement("button");
+    favButton.appendChild(document.createTextNode("Add to Favorites"));
+    buttons.appendChild(favButton);
+
+    const historyButton = document.createElement("button");
+    historyButton.appendChild(document.createTextNode("Stock History Data"));
+    buttons.appendChild(historyButton);
+
 
 })
