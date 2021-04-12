c89cbf090cd39073dad7211eaedc11e038af1740
cassyvan <cvan063@mtroyal.ca>
2021-04-11 20:53:39 -0600

    about page almost completed

--- includes/config.inc.php
+++ includes/config.inc.php
@@ -1,7 +1,7 @@
 <?php
 define('DBHOST', 'localhost');
 define('DBNAME', 'stocks');
-define('DBUSER', 'root');
-define('DBPASS', '');
+define('DBUSER', 'debian-sys-maint');
+define('DBPASS', 'x7xKjs0gs3A1mONc');
 define('DBCONNSTRING', "mysql:host=" . DBHOST . ";dbname=" . DBNAME .
     ";charset=utf8mb4;");
