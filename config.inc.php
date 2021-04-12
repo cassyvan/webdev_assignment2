d80e6ce833b0b2d8c7e1d372f416d9ed16080ea5
mbao019 <mbao019@mtroyal.ca>
2021-04-12 17:09:54 -0600

    she hosted~!

--- config.inc.php
+++
@@ -1,6 +0,0 @@
-<?php
-define('DBHOST', 'td5l74lo6615qq42.cbetxkdyhwsb.us-east-1.rds.amazonaws.com');
-define('DBNAME', 'f05sda46z7l40h58');
-define('DBUSER', 'nn50flcmz5hdkxmh');
-define('DBPASS', 'r5w0jz7yttqbkf3j');
-define('DBCONNSTRING', "mysql:host=" . DBHOST . ";dbname=" . DBNAME . ";charset=utf8mb4;");
