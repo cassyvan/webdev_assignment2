a5db0df35cfdaeb318e9753ea73f21e657b5a9d8
cassyvan <cvan063@mtroyal.ca>
2021-04-13 16:27:25 -0600

    minor changes

--- about.php
+++ about.php
@@ -27,19 +27,23 @@ if (isset($_SESSION["loggedin"]) && $_SESSION["loggedin"] == true) {
 displayNav(false, $check);
 
 echo "<h1 class='about'>About Us</h1>";
-echo "<p>This site is created for Mount Royal University's COMP 3512 (Web Development II) Assignment #2. </p> </br>
-      <p>Professor: Randy Connolly </p></br>
-      <p>Winter 2021 </p></br>
+echo "<h3> Description </h3>
+      <p>This site is created for Mount Royal University's COMP 3512 (Web Development II) Assignment #2.</p>
+      <h3> Professor </h3>
+      <p>Randy Connolly</p>
+      <h3> Semester </h3>
+      <p>Winter 2021 </p>
       <h3>Technologies Used</h3>
       <p>Logo maker: https://www.freelogodesign.org</p>
       <p>Login form: https://codepen.io/colorlib/pen/rxddKy</p>
+      <p>Hosting Website: Heroku </p>
+      <p>Database Hosting: JawsDB </p>
       <h3> Group Members </h3>
       <p id='aboutDescription'>
       <a href='https://github.com/larte834'>Lidiya Artemenko</a><br>
       <a href='https://github.com/mbao019'>Minh Bao</a><br>
       <a href='https://github.com/landyram'>Randy Lam</a><br>
       <a href='https://github.com/cassyvan'>Cassy Van</a><br>
-      <p>
       <p id='aboutDescription'>
       The main assignment github repo can be found <a href='https://github.com/cassyvan/webdev_assignment2'>here</a>
 </p>"; 
