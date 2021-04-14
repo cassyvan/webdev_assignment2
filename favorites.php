3a087a510b1c5acf08f9e328f273f646ba807653
cassyvan <cvan063@mtroyal.ca>
2021-04-13 20:35:32 -0600

    favorites fixed

--- favorites.php
+++ favorites.php
@@ -35,78 +35,70 @@ displayNav(false, $check);
         <?php
         if ($check) {
           try {
-            $url = 'Location: favorites.php';
             $conn = DatabaseHelper::createConnection(array(
               DBCONNSTRING,
               DBUSER, DBPASS
             ));
-            //check to see if "remove all" is set, if it is, then destroy the favorites session
+            $url = 'Location: favorites.php';
+            //check to see if "remove all" is set, if it is, then destroy the old session for favorites
+            //unset method documentation found on https://www.php.net/manual/en/function.session-unset.php
             if (isset($_GET['destroy'])) {
-              foreach ($_SESSION["favorites"] as $key => $value) {
-                unset($_SESSION['favorites'][$key]);
-              }
-              session_write_close();
+              unset($_SESSION['favorites']);
+              header($url);
             }
-            //checks to see if company query string is set and if favorites session is set, if not, start a new session 
             if (isset($_GET['company'])) {
+              //check to see if we favorites stored yet, if not start a new list 
               if (!isset($_SESSION['favorites'])) {
                 $_SESSION['favorites'] = [];
-              } 
+              }
               $companyGateway = new CompaniesDB($conn);
               $company = $companyGateway->getSingleCompany($_GET['company']);
               $company = $company[0];
-              //checks to see if the company already exists in the session array, if not, add it to the favorites session
+              //checks to see if the company already exists in the session array
               if (!in_array($company, $_SESSION['favorites'])) {
                 $fav = $_SESSION["favorites"];
                 $fav[] = $company;
                 $_SESSION['favorites'] = $fav;
               }
             }
-
-            //if session does not exist, or list is empty, then display an error message, else create the Remove All button
-            if (!isset($_SESSION['favorites']) || count($_SESSION["favorites"]) == 0) {
-              echo "<p>You have no favorites set</p>";
-            } else {
+            //if we have a favorites session set, then we output it, otherwise display a message saying no favorites is set
+            if (isset($_SESSION['favorites'])) {
               echo "<form method='post' action='favorites.php?destroy='>";
               echo "<button class='button' id='remAllButton' type='submit' value='all'>Remove All</button>";
               echo "</form>";
+              $fav = $_SESSION["favorites"];
+              //updates and stores the companies into session favorites and then outputs it
+              foreach ($fav as $f => $value) {
+                echo "<li id='fav'>";
+                echo "<img src='logos/" . $value['symbol'] . ".svg' id='favLogo'>";
+                echo "<div id='favNameSymbol'>" . $value['symbol'] . ", ";
+                echo $value['name'] . "</div>";
+                echo "<form method='get' action='favorites.php?remove='>";
+                echo "<button class='button' id='remButton' type='submit' name='remove'" . " value='" . $value['symbol'] . "'>Remove</button>";
+                echo "</form>";
+                echo "</li>";
+              }
+            } else {
+              echo "<p>You have no favorites set</p>";
             }
-           
-            //checks for query string for "remove" to remove a single selected company
-            //this was found on https://board.phpbuilder.com/d/10345796-resolved-remove-item-from-session-array/4
+            //checks for query string for "remove" 
             if (isset($_GET['remove'])) {
+              //check to see if there's only 1 company in the session to remove, if so, we restart the session
+              if (count($fav) == 1) {
+                unset($_SESSION['favorites']);
+                header($url);
+              }
               //loop through the session array to find company to remove
-              foreach ($_SESSION["favorites"] as $key => $value) {
+              foreach ($_SESSION["favorites"] as $key  => $value) {
                 if ($value['symbol'] == $_GET['remove']) {
+                  //this was found on https://board.phpbuilder.com/d/10345796-resolved-remove-item-from-session-array/4
                   unset($_SESSION['favorites'][$key]);
-                  //if there is only 1 company in the session favorites, we restart the session
-                  if (count($fav) == 1) {
-                    header($url);
-                  }
                   //if more than 1 company in the session favorites, we close the session and refresh the page
                   session_write_close();
                   header($url);
                 }
               }
             }
-
-        //if adding favorite from single-company.php, grab the query and add it to the session array
-            if (isset($_SESSION['favorites'])) {
-              $fav = $_SESSION["favorites"];
-              //updates and stores the companies into session favorites and then outputs it
-              foreach ($fav as $f => $value) {
-                echo "<li id='fav'>";
-                echo "<img src='logos/" . $value['symbol'] . ".svg' id='favLogo'>";
-                echo "<div id='favNameSymbol'><a href='single-company.php?symbol=" . $value['symbol'] . "'>" .$value['symbol'] . " </a>";
-                echo "<a href='single-company.php?symbol='" . $value['name'] . "'>" . $value['name'] . "</a></div>";
-                echo "<form method='get' action='favorites.php?remove='>";
-                echo "<button class='button' id='remButton' type='submit' name='remove'" . " value='" . $value['symbol'] . "'>Remove</button>";
-                echo "</form>";
-                echo "</li>";
-              }
-      
-            }
-           
 
           } catch (Exception $e) {
             die($e->getMessage());
